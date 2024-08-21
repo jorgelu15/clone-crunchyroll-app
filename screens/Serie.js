b820ba420f9560841b8888578a02327a88ef620c
Jorge Luis Guardo Romero <deliarosaro112@gmail.com>
2024-08-21 18:26:06 -0500

    se anadio lista de  episodios a la UI de serie

--- screens/Serie.js
+++ screens/Serie.js
@@ -1,6 +1,6 @@
 import { Button, Dimensions, FlatList, Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Feather } from '@expo/vector-icons';
+import { SimpleLineIcons, Entypo, Ionicons, Octicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Rating, AirbnbRating } from 'react-native-ratings';
 
@@ -63,7 +63,7 @@ const Serie = () => {
                     <Text style={{ color: "white" }}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.
                     </Text>
-                    <View style={styles.buttonsContainer}>
+                    {/* <View style={styles.buttonsContainer}>
                         <View style={styles.playButton}>
                             <Feather name="play" size={24} color="black" />
                             <Text style={{ fontWeight: "500", textTransform: "uppercase" }}>Comenzar a ver</Text>
@@ -71,20 +71,47 @@ const Serie = () => {
                         <View style={styles.bookmarkButton}>
                             <Feather name="bookmark" size={24} color="black" />
                         </View>
-                    </View>
-
-                    {/* <View style={styles.recommendationsContainer}>
-                        <Text style={{ color: "#FFFFFF", fontSize: 18, marginBottom: 10 }}>Nuestras recomendaciones</Text>
-                        <FlatList
-                            showsHorizontalScrollIndicator={false}
-                            data={new Array(10).fill("")}
-                            keyExtractor={(item, idx) => idx.toString()}
-                            horizontal={true}
-                            renderItem={({ item, index }) => (
-                                <CardList item={item} />
-                            )}
-                        />
                     </View> */}
+                    <Text style={{ textAlign: "center", color: "#ff0000", fontSize: 16, marginVertical: 10, textTransform: "uppercase", fontWeight: "500" }}>Detalles de la serie</Text>
+                    <View style={{ display: "flex", flexDirection: "column" }}>
+                        <View style={{ display: "flex", flexDirection: "row", marginTop: 10 }}>
+                            <Text style={{ color: "white", paddingVertical: 5, paddingHorizontal: 10, borderBottomColor: "#ff0000", borderBottomWidth: 3 }}>EPISODIOS</Text>
+                            <Text style={{ color: "white", paddingVertical: 5, paddingHorizontal: 10, }}>CONTENIDO SIMILAR  A ESTE</Text>
+                        </View>
+                        <View style={{
+                            display: "flex", flexDirection: "row", justifyContent: "space-between", paddingVertical: 10,
+                            borderTopColor: "#404040", borderTopWidth: 1, borderBottomColor: "#404040", borderBottomWidth: 1
+                        }}>
+                            <View style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                                <Entypo name="chevron-down" size={16} color="white" />
+                                <Text style={{ color: "white", fontSize: 16, marginLeft: 5 }}>Wistoria: Wand and Sword</Text>
+                            </View>
+                            <SimpleLineIcons name="options-vertical" size={16} color="white" />
+                        </View>
+                    </View>
+                    <View style={{ display: "flex", flexDirection: "column" }}>
+                        <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", paddingVertical: 10, borderBottomColor: "#404040", borderBottomWidth: 1 }}>
+                            <View>
+                                <Ionicons name="filter" size={24} color="white" />
+                            </View>
+                            <View style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                                <Text style={{ textTransform: "uppercase", color: "white", paddingHorizontal: 10 }}>Sincronizar todo</Text>
+                                <Octicons name="download" size={24} color="white" />
+                            </View>
+                        </View>
+                        <View style={{ paddingVertical: 10 }}>
+                            <FlatList
+                                data={new Array(10).fill("")}
+                                keyExtractor={(item, idx) => idx.toString()}
+                                renderItem={({ item, idx }) => {
+                                    return (
+                                        <Text style={{ color: "white" }}>Video {idx}</Text>
+                                    )
+                                }}
+                            />
+                        </View>
+
+                    </View>
                 </View>
 
             </ScrollView>
@@ -115,11 +142,16 @@ const styles = StyleSheet.create({
         zIndex: 100,
     },
     contentContainer: {
+        position: "relative",
         paddingHorizontal: 20,
         marginTop: height * 0.60,
         zIndex: 1000,
     },
     buttonsContainer: {
+        position: "absolute",
+        bottom: 0,
+        width: width,
+        paddingHorizontal: 20,
         flexDirection: "row",
         justifyContent: "space-between",
         marginVertical: 15,
